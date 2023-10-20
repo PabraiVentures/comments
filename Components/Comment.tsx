df67f360914e54dcc6f4be3193c2c1ea392610ca
Nathan Pabrai <nathanpabrai@gmail.com>
2023-10-19 23:23:19 -0400

    only comment on leaf nodes

--- Components/Comment.tsx
+++ Components/Comment.tsx
@@ -4,12 +4,12 @@ import { useState } from 'react'
 
 interface CommentProps {
     comment: EnhancedValue
+    depth: number
 }
 
-export default function Comment({ comment }: CommentProps) {
+export default function Comment({ comment, depth }: CommentProps) {
     const [replies, setReplies] = useState<EnhancedValue[]>(comment.children)
     const [newReply, setNewReply] = useState<EnhancedValue>(newEnhancedValue())
-
     const handleReply = () => {
         const updatedReplies = [...replies, newReply]
         setReplies(updatedReplies)
@@ -19,28 +19,30 @@ export default function Comment({ comment }: CommentProps) {
     return (
         <div className='bg-gray-100 p-4 rounded-md my-2'>
             <p className='text-gray-700'>{comment.name}</p>
-            <div className='flex mt-2'>
-                <input
-                    value={newReply.name}
-                    onChange={e => setNewReply({
-                        ...newReply,
-                        name: e.target.value
-                    })}
-                    placeholder='Log this value...'
-                    className='flex-1 p-2 rounded-md border border-gray-300 mr-2'
-                />
-                <button
-                    onClick={handleReply}
-                    className='bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600'
-                >
-                    Reply
-                </button>
-            </div>
             <div className='ml-4 mt-2'>
                 {replies.map((reply, index) => (
-                    <Comment key={index} comment={reply} />
+                    <Comment key={index} comment={reply} depth={depth + 1} />
                 ))}
             </div>
+            {depth == 1 &&
+                <div className='flex mt-2'>
+                    <input
+                        value={newReply.name}
+                        onChange={e => setNewReply({
+                            ...newReply,
+                            name: e.target.value
+                        })}
+                        placeholder='Log this value...'
+                        className='flex-1 p-2 rounded-md border border-gray-300 mr-2'
+                    />
+                    <button
+                        onClick={handleReply}
+                        className='bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600'
+                    >
+                        Reply
+                    </button>
+                </div>
+            }
         </div>
     )
 }
--- Components/CommentList.tsx
+++ Components/CommentList.tsx
@@ -20,28 +20,9 @@ export default function CommentsList() {
 
     return (
         <div className='p-4'>
-            <div className='flex mb-4'>
-                <input
-                    value={newComment.name}
-                    onChange={e => {
-                        setNewComment({
-                            ...newComment,
-                            name: e.target.value
-                        })
-                    }}
-                    placeholder='Add a value...'
-                    className='flex-1 p-2 rounded-md border border-gray-300 mr-2'
-                />
-                <button
-                    onClick={handleAddComment}
-                    className='bg-green-500 text-white rounded-md px-4 py-2 hover:bg-green-600'
-                >
-                    Comment
-                </button>
-            </div>
             <div className='space-y-4'>
                 {comments.map((comment, index) => (
-                    <Comment key={index} comment={comment} />
+                    <Comment key={index} comment={comment} depth={0} />
                 ))}
             </div>
         </div>
